a7f8ba782e787fc27ea8a2666b6ccd94b97c7833
Auzo <akjha96@github.com>
2020-02-07 08:26:58 +0530

    Refined cardList component

--- src/CardList.js
+++ src/CardList.js
@@ -2,10 +2,13 @@ import React, { Fragment } from "react";
 import Card from "./Card";
 
 const CardList = ({ robots }) => {
-  const cardComponent = robots.map((user, i) => {
-    return <Card id={robots[i].id} name={robots[i].name} email={robots[i].email} />;
-  });
-  return <Fragment>{cardComponent}</Fragment>;
+  return (
+    <Fragment>
+      {robots.map((user, i) => {
+        return <Card key={i} id={robots[i].id} name={robots[i].name} email={robots[i].email} />;
+      })}
+    </Fragment>
+  );
 };
 
 export default CardList;
